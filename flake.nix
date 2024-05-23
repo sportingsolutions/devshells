3f745f4ddc697043d1094ca670758afca73d129f
Fabrice Semti <fsemti@sportiinggroup.co.uk>
2024-05-23 08:13:58 +0000

    use flake

--- flake.nix
+++ flake.nix
@@ -5,22 +5,33 @@
     flake-utils.url = "github:numtide/flake-utils";
     nixops-flake.url = "github:input-output-hk/nixops-flake";
     nixpkgs.url = "github:NixOS/nixpkgs";
+    devenv.url = "github:cachix/devenv";
   };
 
-  outputs = { self, nixpkgs, flake-utils, nixops-flake }:
-    flake-utils.lib.eachDefaultSystem
-      (system: let
-        pkgs = nixpkgs.legacyPackages.${system};
-      in
-        {
-          devShell = pkgs.mkShell {
-            buildInputs = with pkgs; [
+  nixConfig = {
+    extra-trusted-public-keys = "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw=";
+    extra-substituters = "https://devenv.cachix.org";
+  };
+
+  outputs = { self, nixpkgs, devenv, ... } @ inputs:
+    let
+      pkgs = nixpkgs.legacyPackages."x86_64-linux";
+    in
+    {
+      devShell.x86_64-linux = devenv.lib.mkShell {
+        inherit inputs pkgs;
+        modules = [
+          ({ pkgs, config, ... }: {
+            # This is your devenv configuration
+            packages = with pkgs; [
+
               # Packages for Ansible
               ansible
               ansible-lint
               glibcLocales
               python3
               python311Packages.dnspython
+              python311Packages.mitogen # https://www.linkedin.com/pulse/how-speed-up-ansible-playbooks-drastically-lionel-gurret          # ❯ python -c "import mitogen; print(mitogen.__file__)"    
               python311Packages.pykerberos
               python311Packages.pip
               python311Packages.pyspnego
@@ -46,20 +57,33 @@
               go-task
               htop 
               jq 
+              just
               tree
               tmux
               whois
 
               # Developer tools
               niv
+              nixfmt-rfc-style
+              nixfmt
               pre-commit
+
+              # Terminal
               zsh
+              zellij
 
               # Default packages from Nixpkgs
               hello
-              nixops-flake.defaultPackage.${system}
+              # nixops-flake.defaultPackage.${system}
             ];
-          };
-        }
-      );
+
+            enterShell = ''
+              hello
+            '';
+
+            processes.run.exec = "hello";
+          })
+        ];
+      };
+    };
 }
